b3b5c3112619226e0ae5e46aa8d3a1248e499101
rafin8990 <rafin8990@gmail.com>
2023-06-09 20:46:21 +0600

    video added

--- src/Pages/LandingPage/VideoSection/VideoSection.jsx
+++ src/Pages/LandingPage/VideoSection/VideoSection.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import "./VideoSection.css";
 
 const VideoSection = () => {
+  const videoId = "iir3tMlmiWg&list";
   return (
-    <div>
-      <video
-        className=" w-full"
-        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-        controls
-      />
+    <div className="video-container">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube-nocookie.com/embed/vtG1jwJRGzU"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 };
